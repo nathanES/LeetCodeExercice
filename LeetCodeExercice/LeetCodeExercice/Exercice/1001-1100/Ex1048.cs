dc1aa9818df10d95f1b47e07921609ca785b96ba
nathan <nathan.esslinger67@gmail.com>
2023-09-23 18:33:36 +0200

    Ex1048

---
+++ LeetCodeExercice/LeetCodeExercice/Exercice/1001-1100/Ex1048.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeExercice.Exercice._1001_1100;
+
+public class Ex1048
+{
+    public Ex1048()
+    {
+        
+    }
+    public int LongestStrChain(string[] words)
+    {
+        Array.Sort(words, (a, b) => a.Length.CompareTo(b.Length));
+        Dictionary<string, int> dp = new Dictionary<string, int>();
+        int max_chain = 0;
+        foreach (var word in words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.Length; i++) {
+                string prev_word = word.Remove(i, 1);
+                if (dp.ContainsKey(prev_word)) {
+                    dp[word] = Math.Max(dp[word], dp[prev_word] + 1);
+                }
+            }
+            max_chain = Math.Max(max_chain, dp[word]);
+        }
+        return max_chain;
+    }
+}
