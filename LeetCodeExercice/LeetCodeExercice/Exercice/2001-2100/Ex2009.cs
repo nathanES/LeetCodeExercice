9e88d4ba5b1c3de23c5cf5e80bdaaf3cde7d807c
nathan <nathan.esslinger67@gmail.com>
2023-10-10 18:13:28 +0200

    Ex2009

---
+++ LeetCodeExercice/LeetCodeExercice/Exercice/2001-2100/Ex2009.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeExercice;
+
+public class Ex2009
+{
+    public Ex2009()
+    {
+        
+    }
+        public int MinOperations(int[] nums) {
+         Array.Sort(nums);
+        List<int> list = new List<int>();
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            list.Add(nums[i]);
+        }
+
+        int max = 0;
+        
+        for (int i = 0; i < list.Count; i++)
+        {
+            int target = list[i] + nums.Length - 1;
+            int index = list.BinarySearch(target);
+            
+            if (index < 0)
+                index = (~index) - 1;
+            
+            max = Math.Max(max, index - i + 1);
+        }
+        
+        return nums.Length - max;
+    }
+}
